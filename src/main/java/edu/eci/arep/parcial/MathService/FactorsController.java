699d35a5228ec9ca2f6eca58f6b44d5d3d4a413a
unknown <nicolas.bernal-f@labinfo.is.escuelaing.edu.co>
2025-03-28 10:53:46 -0500

    Realizando la operacion de factores

--- src/main/java/edu/eci/arep/parcial/MathService/FactorsController.java
+++ src/main/java/edu/eci/arep/parcial/MathService/FactorsController.java
@@ -1,8 +1,35 @@
 package edu.eci.arep.parcial.MathService;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class FactorsController {
+    @GetMapping("/factors")
+    public ResponseEntity<Map<String, Object>>factors(@RequestParam("value")int value){
 
+        List<Integer>list=getFactors(value);
+        Map<String, Object>response = new HashMap<>();
+        response.put("operation", "factors");
+        response.put("input", value);
+        response.put("output", list);
+        return ResponseEntity.ok(response);
+    }
+
+    private List<Integer> getFactors(int num){
+        List<Integer> factors=new ArrayList<>();
+        for(int i=1; i<= num; i++){
+            if(num%i==0){
+                factors.add(i);
+            }
+        }
+        return factors;
+    }
 }
